0136aa27aa036b883432500a95c82e485e172af7
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-11-30 11:26:53 -0600

    commit tree 83c2f548f433a98c662e543fdf80f82004246b26

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+# require "pry"
+
+class Anagram
+    attr_reader :word
+
+    def initialize(input)
+        @word = input
+    end
+
+    def match(arr)
+        word_letters = @word.split("")
+        arr.filter { |wd| wd.split("").sort == word_letters.sort }
+    end
+end
+
+# binding.pry
+diaper = Anagram.new('diaper')
+diaper.match(%w[hello world zombies pants dipper])
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
+# listen.match(["enlists", "google", "inlets", "banana"])
+
+ba = Anagram.new('ba')
+ba.match(%w[ab abc bac'])
+
+allergy = Anagram.new('allergy')
+allergy.match(%w[gallery ballerina regally clergy largely leading])
